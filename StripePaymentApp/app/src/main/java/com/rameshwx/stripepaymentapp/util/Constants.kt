404e47304d2bfb0c55de715d1d1e833c2893c643
daminduXONT <rameshwx@gmail.com>
2025-03-07 20:01:12 +0530

    1.0

--- StripePaymentApp/app/src/main/java/com/rameshwx/stripepaymentapp/util/Constants.kt
+++ StripePaymentApp/app/src/main/java/com/rameshwx/stripepaymentapp/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.rameshwx.stripepaymentapp.util
 
 object Constants {
-    const val STRIPE_PUBLISHABLE_KEY = "pk_test_51ImhUEGRdQfhxaSJOCyltUeJL5VtoBPxc8ysIqJdxx6tCsIBXXIH2y42BtVMLIW0iYrVIufFY3XL6ZmPAXzbqHHx00NXlqbFjZ" // Replace with your test key
+    const val STRIPE_PUBLISHABLE_KEY = "pk_test_51ImhUEGRdQfhxaSJOCyltUeJL5VtoBPxc8ysIqJdxx6tCsIBXXIH2y42BtVMLIW0iYrVIufFY3XL6ZmPAXzbqHHx00NXlqbFjZ"
 }
